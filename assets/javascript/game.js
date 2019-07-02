b41062f33d9d988dcb10e8d48734fff3de9198d1
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-02 09:59:16 -0400

    added image function clickable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     var random = Math.floor(Math.random() * 101 + 19);
     $("#randomNo").text(random);
 
-    var crys1 = Math.floor(Math.random() * 11 + 1)
-    var crys2 = Math.floor(Math.random() * 11 + 1)
-    var crys3 = Math.floor(Math.random() * 11 + 1)
-    var crys4 = Math.floor(Math.random() * 11 + 1)
+    var crys1 = Math.floor(Math.random() * 11 + 1);
+    var crys2 = Math.floor(Math.random() * 11 + 1);
+    var crys3 = Math.floor(Math.random() * 11 + 1);
+    var crys4 = Math.floor(Math.random() * 11 + 1);
 
     var total = 0;
     var wins = 0;
@@ -16,7 +16,6 @@ $(document).ready(function () {
 
     function reset() {
         random = Math.floor(Math.random() * 101 + 19);
-        console.log(random)
         $('#randomNo').text(random);
         crys1 = Math.floor(Math.random() * 11 + 1);
         crys2 = Math.floor(Math.random() * 11 + 1);
@@ -34,49 +33,13 @@ $(document).ready(function () {
     function lost() {
         alert("You lose!");
         losses++;
-        $('#totalLosses').text(losses);
+        $("#totalLosses").text(losses);
         reset()
     }
-    $('#blue').on('click', function () {
+    $(document).ready(function(){
+    $("#blue").click(function() {
         total = total + crys1;
-        console.log("New userTotal= " + total);
-        $('#finalTotal').text(total);
-
-        if (total == random) {
-            won();
-        }
-        else if (total > random) {
-            lost();
-        }
-    })
-    $('#red').on('click', function () {
-        total = total + crys2;
-        console.log("New userTotal= " + total);
-        $('#finalTotal').text(total);
-        if (total == random) {
-            won();
-        }
-        else if (total > random) {
-            lost();
-            console.log(total)
-        }
-    })
-    $('#green').on('click', function () {
-        total = total + crys3;
-        console.log("New userTotal= " + total);
-        $('#finalTotal').text(total);
-        //sets win/lose conditions
-        if (total == random) {
-            won();
-        }
-        else if (total > random) {
-            lost();
-        }
-    })
-    $('#purple').on('click', function () {
-        total = total + crys4;
-        console.log("New userTotal= " + total);
-        $('#finalTotal').text(total);
+        $("#finalTotal").text(total);
 
         if (total == random) {
             won();
@@ -85,4 +48,36 @@ $(document).ready(function () {
             lost();
         }
     });
-})
+    console.log(click)
+});
+    $("#red").on("click", function () {
+        total = total + crys2;
+        $("#finalTotal").text(total);
+        if (total == random) {
+            won();
+        }
+        else if (total > random) {
+            lost();
+        }
+    })
+    $("#green").on("click", function () {
+        total = total + crys3;
+        $("#finalTotal").text(total);
+        if (total == random) {
+            won();
+        }
+        else if (total > random) {
+            lost();
+        }
+    })
+    $("#purple").on("click", function () {
+        total = total + crys4;
+        $("#finalTotal").text(total);
+        if (total == random) {
+            won();
+        }
+        else if (total > random) {
+            lost();
+        }
+    });
+});
